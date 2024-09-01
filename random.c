a661d2c7ca1131625efeff2638ad2a7d357cec16
apengskiii <tui59806@temple.edu>
2024-09-01 19:12:13 -0400

    Delete random.c

--- random.c
+++
@@ -1,7 +0,0 @@
-//random.c that contains the function randchar()
-
-#include <stdlib.h>
-
-char randchar() {  //function that returns a random character 'A' to 'Z' 
-  return 'A' + (rand() % 26);
-}
